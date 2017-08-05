a46215b687f1cafffb0ef7e5549e168eb53d61e0
Paolo Puggioni <p.paolo321@gmail.com>
2017-08-05 22:13:11 +0100

    add crossreference plots

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from os import listdir
 from collections import OrderedDict
 from hrate.data_handling.selfloops import read_selfloops_file
 
-
 app = dash.Dash()
 
 # LOADING data
@@ -23,110 +22,97 @@ for f in file_paths:
 available_files = list(data.keys())
 
 app.layout = html.Div([
-    html.Div([
 
-        html.Div([
-            dcc.Dropdown(
-                id='file_name',
-                options=[{'label': i, 'value': i} for i in available_files],
-                value=available_files[0]
-            ),
-            dcc.RadioItems(
-                id='rr-xaxis-name',
-                options=[{'label': i, 'value': i} for i in ['Timestamp', 'Time Lapsed']],
-                value='Timestamp',
-                labelStyle={'display': 'inline-block'}
-            )
-        ],
-            style={'width': '48%', 'display': 'inline-block'}),
+    html.Div([
+        dcc.Dropdown(
+            id='file_name',
+            options=[{'label': i, 'value': i} for i in available_files],
+            value=available_files[0]
+        )
     ]),
 
     dcc.Graph(id='HR_plot'),
 
-    # dcc.Graph(id='RR_plot'),
-
+    dcc.Graph(id='RR_plot')
 
 ])
 
 
+@app.callback(
+    dash.dependencies.Output('RR_plot', 'figure'),
+    [dash.dependencies.Input('file_name', 'value'),
+     dash.dependencies.Input('HR_plot', 'selectedData'),
+     ])
+def update_RR_graph(file_name, x_selected_hr):
+    if x_selected_hr is None:
+        dff = data[file_name].head(0)
+    else:
+        x_stamp_min = pd.to_datetime(x_selected_hr['range']['x'][0])
+        x_stamp_max = pd.to_datetime(x_selected_hr['range']['x'][1])
 
+        x_stamp_max = min(x_stamp_min + pd.to_timedelta(300, unit='s'), x_stamp_max)
 
-# @app.callback(
-#     dash.dependencies.Output('RR_plot', 'figure'),
-#     [dash.dependencies.Input('file_name', 'value'),
-#      dash.dependencies.Input('xaxis-name', 'value')])
-# def update_RR_graph(file_name, xaxis):
-#     dff = data[file_name]
-#     if xaxis == 'Timestamp':
-#         x_col = 'Time_stamp'
-#     elif xaxis == 'Lapsed Time':
-#         x_col = 'Time_lapsed'
-#     else:
-#         raise Exception("xaxis can be only ['Timestamp', 'Time Lapsed']")
-#
-#     return {
-#         'data': [go.Scatter(
-#             x=dff[x_col],
-#             y=dff['RR'],
-#             mode='markers',
-#             marker={
-#                 'size': 3,
-#                 'opacity': 0.5,
-#                 'line': {'width': 0.5, 'color': 'white'}
-#             }
-#         )],
-#         'layout': go.Layout(
-#             xaxis={
-#                 "rangeselector": {
-#                     "buttons": [
-#                         {
-#                             "count": 5,
-#                             "step": "minute",
-#                             "stepmode": "backward",
-#                             "label": "5M"
-#                         },
-#                         {
-#                             "count": 30,
-#                             "step": "minute",
-#                             "stepmode": "backward",
-#                             "label": "30M"
-#                         },
-#                         {
-#                             "count": 1,
-#                             "step": "hour",
-#                             "stepmode": "backward",
-#                             "label": "1H"
-#                         },
-#                         {
-#                             "count": 4,
-#                             "step": "hour",
-#                             "stepmode": "backward",
-#                             "label": "4H"
-#                         },
-#                         {
-#                             "step": "all"
-#                         }
-#                     ]
-#                 },
-#                 "rangeslider": {},
-#                 "type": "date"
-#             },
-#             yaxis={
-#                 'title': 'RR intervals (ms)',
-#             },
-#             margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
-#             hovermode='closest'
-#         )
-#     }
+        idx = (data[file_name]['Time_stamp'] > x_stamp_min) & \
+              (data[file_name]['Time_stamp'] < x_stamp_max)
+        dff = data[file_name].loc[idx]
+
+    return {
+        'data': [go.Scatter(
+            x=dff['Time_stamp'],
+            y=dff['RR'],
+            mode='lines+markers',
+            marker={
+                'size': 5,
+                'opacity': 1,
+                'color': ('rgb(22, 96, 167)'),
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            line={
+                'color':('rgb(30, 30, 30)'),
+                'width': 0.3,
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={
+                "rangeselector": {
+                    "buttons": [
+                        {
+                            "count": 5,
+                            "step": "minute",
+                            "stepmode": "backward",
+                            "label": "5m"
+                        },
+                        {
+                            "count": 10,
+                            "step": "minute",
+                            "stepmode": "backward",
+                            "label": "10m"
+                        },
+                        {
+                            "step": "all"
+                        }
+                    ]
+                },
+                "rangeslider": {},
+                "type": "date"
+            },
+            yaxis={
+                'title': 'RR intervals (ms)',
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    }
 
 
 def resample_df(df):
     if len(df.index) < 1000:
         return df
 
-    step = int(len(df.index)/1000)
-    idx_keep = np.arange(0,len(df.index),step)
+    step = int(len(df.index) / 1000)
+    idx_keep = np.arange(0, len(df.index), step)
     return df.loc[idx_keep]
+
 
 @app.callback(
     dash.dependencies.Output('HR_plot', 'figure'),
@@ -140,10 +126,14 @@ def update_HR_graph(file_name):
         'data': [go.Scatter(
             x=dff['Time_stamp'],
             y=dff['HR'],
-            mode='line',
-            line=dict(
-                color=('rgb(205, 12, 24)'),
-                width=2)
+            mode='lines+markers',
+            line={
+                'color': ('rgb(205, 12, 24)'),
+                'width': 2
+            },
+            marker={
+                'size': 4
+            }
         )],
         'layout': go.Layout(
             xaxis={
@@ -153,25 +143,25 @@ def update_HR_graph(file_name):
                             "count": 5,
                             "step": "minute",
                             "stepmode": "backward",
-                            "label": "5M"
+                            "label": "5m"
                         },
                         {
                             "count": 30,
                             "step": "minute",
                             "stepmode": "backward",
-                            "label": "30M"
+                            "label": "30m"
                         },
                         {
                             "count": 1,
                             "step": "hour",
                             "stepmode": "backward",
-                            "label": "1H"
+                            "label": "1h"
                         },
                         {
                             "count": 4,
                             "step": "hour",
                             "stepmode": "backward",
-                            "label": "4H"
+                            "label": "4h"
                         },
                         {
                             "step": "all"
@@ -179,8 +169,7 @@ def update_HR_graph(file_name):
                     ]
                 },
                 "rangeslider": {},
-                "type": "date",
-                "title": "Timestamp"
+                "type": "date"
             },
             yaxis={
                 'title': 'Heart Rate (bpm)',
