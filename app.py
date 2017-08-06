9d1fc89c27b5c1e7683e382c92437ad7f7dd0cd0
Paolo Puggioni <p.paolo321@gmail.com>
2017-08-06 22:54:33 +0100

    add histogram - it has bugs

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ app.layout = html.Div([
         )
     ]),
 
-    dcc.Graph(id='HR_plot'),
+    html.Div([dcc.Graph(id='HR_plot')], style={'width': '60%', 'display': 'inline-block', 'padding': '0 20'}),
+    html.Div([dcc.Graph(id='HR_summary')], style={'width': '25%', 'display': 'inline-block'}),
 
     dcc.Graph(id='RR_plot')
 
@@ -39,91 +40,34 @@ app.layout = html.Div([
 
 
 @app.callback(
-    dash.dependencies.Output('RR_plot', 'figure'),
-    [dash.dependencies.Input('file_name', 'value'),
-     dash.dependencies.Input('HR_plot', 'selectedData'),
-     ])
-def update_RR_graph(file_name, x_selected_hr):
-    if x_selected_hr is None:
-        dff = data[file_name].head(0)
-    else:
-        x_stamp_min = pd.to_datetime(x_selected_hr['range']['x'][0])
-        x_stamp_max = pd.to_datetime(x_selected_hr['range']['x'][1])
-
-        x_stamp_max = min(x_stamp_min + pd.to_timedelta(300, unit='s'), x_stamp_max)
-
-        idx = (data[file_name]['Time_stamp'] > x_stamp_min) & \
-              (data[file_name]['Time_stamp'] < x_stamp_max)
-        dff = data[file_name].loc[idx]
-
-    return {
-        'data': [go.Scatter(
-            x=dff['Time_stamp'],
-            y=dff['RR'],
-            mode='lines+markers',
-            marker={
-                'size': 5,
-                'opacity': 1,
-                'color': ('rgb(22, 96, 167)'),
-                'line': {'width': 0.5, 'color': 'white'}
-            },
-            line={
-                'color':('rgb(30, 30, 30)'),
-                'width': 0.3,
-            }
-        )],
-        'layout': go.Layout(
-            xaxis={
-                "rangeselector": {
-                    "buttons": [
-                        {
-                            "count": 5,
-                            "step": "minute",
-                            "stepmode": "backward",
-                            "label": "5m"
-                        },
-                        {
-                            "count": 10,
-                            "step": "minute",
-                            "stepmode": "backward",
-                            "label": "10m"
-                        },
-                        {
-                            "step": "all"
-                        }
-                    ]
-                },
-                "rangeslider": {},
-                "type": "date"
-            },
-            yaxis={
-                'title': 'RR intervals (ms)',
-            },
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
-            hovermode='closest'
-        )
-    }
-
-
-def resample_df(df):
-    if len(df.index) < 1000:
-        return df
-
-    step = int(len(df.index) / 1000)
-    idx_keep = np.arange(0, len(df.index), step)
-    return df.loc[idx_keep]
-
-
-@app.callback(
     dash.dependencies.Output('HR_plot', 'figure'),
-    [dash.dependencies.Input('file_name', 'value')])
-def update_HR_graph(file_name):
+    [dash.dependencies.Input('file_name', 'value'),
+     dash.dependencies.Input('RR_plot', 'figure')])
+def update_HR_graph(file_name, rr_plot):
     dff = data[file_name]
+
+    # get range of RR, to be drawn here
+    plot_data = []
+    if 'range' in rr_plot['layout']['xaxis'].keys():
+        RR_plot_range = rr_plot['layout']['xaxis']['range']
+        y_max = dff['HR'].max()
+        y_min = dff['HR'].min()
+
+        RR_range_plot = go.Scatter(
+            x=RR_plot_range,
+            y=[y_max, y_max],
+            fill='tozeroy',
+            mode='lines',
+            line=dict(
+                color='rgb(80, 80, 80)',
+            ),
+            showlegend=False,
+        )
+        plot_data.append(RR_range_plot)
 
     dff = resample_df(dff)
 
-    return {
-        'data': [go.Scatter(
+    HR_data = go.Scatter(
             x=dff['Time_stamp'],
             y=dff['HR'],
             mode='lines+markers',
@@ -133,8 +77,13 @@ def update_HR_graph(file_name):
             },
             marker={
                 'size': 4
-            }
-        )],
+            },
+        showlegend=False
+        )
+    plot_data.append(HR_data)
+
+    figure_hr_plot = {
+        'data': plot_data,
         'layout': go.Layout(
             xaxis={
                 "rangeselector": {
@@ -179,5 +128,139 @@ def update_HR_graph(file_name):
         )}
 
 
+
+
+    return figure_hr_plot
+
+
+
+@app.callback(
+    dash.dependencies.Output('HR_summary', 'figure'),
+    [dash.dependencies.Input('file_name', 'value'),
+     dash.dependencies.Input('HR_plot', 'figure'),
+     ])
+def update_hr_summary_figure(file_name, HR_plot_figure):
+    # TODO: do create and update!
+    # TODO: histogram is wrong, it needs to have resampled uniform data on the x-axis!!
+    dff = data[file_name]
+
+    plot_data = [go.Histogram(
+        y = dff['HR'].values,
+        marker=dict(
+            color='#EB89B5'
+        ),
+        histnorm='probability',
+        name='All data',
+        opacity=0.6
+    )]
+    # TODO: bug! this is called rarely and does not work, because the range is not the xaxis lim!!!
+    if 'range' in HR_plot_figure['layout']['xaxis'].keys():
+        HR_plot_range = HR_plot_figure['layout']['xaxis']['range']
+        idx = (data[file_name]['Time_stamp'] > pd.to_datetime(HR_plot_range[0])) & \
+              (data[file_name]['Time_stamp'] < pd.to_datetime(HR_plot_range[1]))
+        plot_selected_data = go.Histogram(
+            y=dff.loc[idx, 'HR'].values,
+            marker=dict(
+                color='#FFD7E9'
+            ),
+            histnorm='probability',
+            name='Selected range',
+            opacity=0.6
+        )
+        plot_data.append(plot_selected_data)
+
+    return {'data': plot_data,
+            'layout': go.Layout(
+                title='HR (bpm)',
+                barmode='overlay'
+            )
+            }
+
+
+@app.callback(
+    dash.dependencies.Output('RR_plot', 'figure'),
+    [dash.dependencies.Input('file_name', 'value'),
+     dash.dependencies.Input('HR_plot', 'selectedData'),
+     ])
+def update_RR_graph(file_name, x_selected_hr):
+    if x_selected_hr is None:
+        dff = data[file_name].head(0)
+    else:
+        x_stamp_min = pd.to_datetime(x_selected_hr['range']['x'][0])
+        x_stamp_max = pd.to_datetime(x_selected_hr['range']['x'][1])
+
+        x_stamp_max = min(x_stamp_min + pd.to_timedelta(300, unit='s'), x_stamp_max)
+
+        idx = (data[file_name]['Time_stamp'] > x_stamp_min) & \
+              (data[file_name]['Time_stamp'] < x_stamp_max)
+        dff = data[file_name].loc[idx]
+
+
+    RR_data = go.Scatter(
+            x=dff['Time_stamp'],
+            y=dff['RR'],
+            text=dff['HR'],
+            mode='lines+markers',
+            marker={
+                'size': 5,
+                'opacity': 1,
+                'color': ('rgb(22, 96, 167)'),
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            line={
+                'color':('rgb(30, 30, 30)'),
+                'width': 0.3,
+            }
+        )
+
+
+
+    return {
+        'data': [RR_data],
+        'layout': go.Layout(
+            xaxis={
+                "rangeselector": {
+                    "buttons": [
+                        {
+                            "count": 5,
+                            "step": "minute",
+                            "stepmode": "backward",
+                            "label": "5m"
+                        },
+                        {
+                            "count": 10,
+                            "step": "minute",
+                            "stepmode": "backward",
+                            "label": "10m"
+                        },
+                        {
+                            "step": "all"
+                        }
+                    ]
+                },
+                "rangeslider": {},
+                "type": "date"
+            },
+            yaxis={
+                'title': 'RR intervals (ms)',
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+
+def resample_df(df):
+    if len(df.index) < 1000:
+        return df
+
+    step = int(len(df.index) / 1000)
+    idx_keep = np.arange(0, len(df.index), step)
+    return df.loc[idx_keep]
+
+
+
+
 if __name__ == '__main__':
+    app.config.supress_callback_exceptions = True
     app.run_server()
